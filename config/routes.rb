579cd688e1de2fa782d385147ac303dc3b39efb4
Satoshi Nagasawa <nagasawa@ham.works>
2024-04-24 09:23:56 +0900

    add: post update

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # localhost:3000/posts
-  resources :posts, only: [:index, :new, :create]
+  resources :posts, only: [:index, :new, :create, :edit, :update, :destroy]
 end
